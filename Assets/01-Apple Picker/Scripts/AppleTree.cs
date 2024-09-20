6f2f2d8be68ab1ccb874b039b930cd30e0bf1262
Tamer El Gayar <amor@localhost.localdomain>
2024-09-19 22:28:07 -0400

    Apple Picker Prototype part 1

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondBetweenAppleDrops = 1f;
+
+    void Start() {
+
+    }
+
+    void Update () {
+        
+    }
+
+
+    
+}
