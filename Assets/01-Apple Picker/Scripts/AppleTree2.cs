653585c84258480a3f9c583ceca93995b27fc9cd
Tamer El Gayar <amor@localhost.localdomain>
2024-09-21 00:03:22 -0400

    adding movement to script

---
+++ Assets/01-Apple Picker/Scripts/AppleTree2.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+
+    public float speed = 10;
+
+    public float leftAndRightEdge = 20f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondBetweenAppleDrops = 1f;
+
+    void Start() {
+        Invoke ("DropApple", 2f);
+
+    }
+
+
+
+    void Update () {
+        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x <- leftAndRightEdge) {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed);
+        } else if (Random.value < chanceToChangeDirections) {
+            speed *= -1;
+        }
+    }
+
+
+    
+}
