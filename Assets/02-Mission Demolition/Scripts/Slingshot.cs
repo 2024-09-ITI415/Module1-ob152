ceb135125ac14aa6a25ad909c001931593a773c3
amor <ob152@rutgers.edu>
2024-10-10 19:26:53 -0400

    Script Fixes

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseEnter() 
+    {
+        print("Slingshot:OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
