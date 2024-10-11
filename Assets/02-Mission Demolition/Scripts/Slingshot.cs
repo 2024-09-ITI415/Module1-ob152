3e478e5ad956a6aea0ed5b70c536931e3aeaf45f
amor <ob152@rutgers.edu>
2024-10-10 20:00:54 -0400

    Making Things Fly

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -66,6 +66,8 @@ public class Slingshot : MonoBehaviour
         mousePos2D.z = -Camera.main.transform.position.z;
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
 
+        Vector3 mouseDelta = mousePos3D-launchPos;
+
         float maxMagnitude = this.GetComponent<SphereCollider>().radius;
         if (mouseDelta.magnitude > maxMagnitude) {
              mouseDelta.Normalize();
