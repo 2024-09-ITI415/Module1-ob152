57712e3ad0fd6e7c922453330e8b3a437e40effe
Omar Battat <ob152@rutgers.edu>
2024-11-01 22:41:03 -0400

    finishing proto

--- Assets/03-Prototype1/Scripts/Egg.cs
+++ Assets/03-Prototype1/Scripts/Egg.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
-
-public class Egg
+using UnityEngine.SceneManagement;
+public class Egg : MonoBehaviour
 {
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Congratulatios! You found the Egg and won!");
+        }
+    }
 }
